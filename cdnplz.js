b2be718dc85cdcd9b76ebb3764cd09e54304fa8d
zhouwei3-xy <zhouwei3-xy@360.cn>
2016-08-15 12:31:29 +0800

    done

--- cdnplz.js
+++ cdnplz.js
@@ -12,8 +12,7 @@ var cdnplz = {
         output_path: 'output/view/', //输出目录
         file_encoding: 'utf8', //文件编码
         cdn_provider: '',
-        cdn_provider_upload: 'upload',
-        plugins: {}
+        cdn_options: {}
     },
     jadeTypeMap: { // 标签与属性的对应关系
         'img': 'src',
@@ -27,18 +26,20 @@ var cdnplz = {
     cdnCacheFileName: './cdn.cache',// 本地的CDN地址缓存文件路径+名称
     resourceTree: [],// 缓存 CDN 文件上传，避免重复上传
     cdnProvider: null,// 用户自定义CDN Provider
+    tempPath: '',//存放临时文件
     startTime: 0, //记录总用时的start
     init(options) {
         this.startTime = new Date().getTime();
         this.options = Object.assign(this.options, options); // 用户配置覆盖默认配置
+        this.tempPath = require("os").tmpdir(); //获取文件临时文件夹路径
         if(!this.checkOption('tpl_path') || !this.checkOption('static_path') || !this.checkOption('cdn_provider')) {
             return false;
         }
-        this.cdnProvider = require(this.options.cdn_provider);
+        this.cdnProvider = require('cdnplz-'+this.options.cdn_provider);
         try { //读取cdn.cache文件，返回一个json格式文件，key: md5, value: cdn 地址
             this.cdnCacheFromFile = JSON.parse(fs.readFileSync(this.cdnCacheFileName, 'utf8'));
         }catch(e){
-            console.log(e);
+            //console.log(e); 若没有缓存文件，不报错
         }
         this.start();
     },
@@ -62,8 +63,12 @@ var cdnplz = {
         console.log(JSON.stringify(this.resourceTree));
         // 遍历资源树，处理其中的子资源
         promises = this.resourceTree.map(res =>
-            this._dealSubResource(res).then(data =>
+            this._dealSubResource(res).then(data =>{
+                console.log('--------------data----------');
+                console.log(data);
+                console.log('--------------data----------');
                 this._saveFile(res.fileName, this._replace(fs.readFileSync(this._getWholePathFile(res.fileName), 'utf8'), data))
+            }
             ).catch(e => console.log(e))
         );
         // cdn 上传结束
@@ -81,6 +86,7 @@ var cdnplz = {
         );
 
         return Promise.all(promises).then(response => {
+            console.dir(response);
             response.forEach(r => {// 处理response，将文件缓存
                 for( var fileName in r ){
                     this.cdnCacheFromFile[this._md5FileSync(fileName)] = r[fileName];
@@ -88,6 +94,7 @@ var cdnplz = {
             });
             if(this._getFileSuffix(res.fileName) === 'css'){// 替换CSS文件中的资源地址
                 var cssContent = fs.readFileSync(this._getWholePathFile(res.fileName), 'utf8');
+                console.log(res.fileName);
                 this._saveFile(res.fileName, this._replace(cssContent, response));
                 return this._uploadFile(res.fileName);
             } else {
@@ -110,10 +117,12 @@ var cdnplz = {
     //写入指定 output 文件
     _saveFile(file, fileContent)  {
         var outputFile = (this._getFileSuffix(file) == 'css')
-                         ? (this.options.static_path + file)
+                         ? (this.tempPath + file)
                          : (file.replace(this.options.tpl_path, this.options.output_path));
         try{
-            fs.statSync(path.dirname(outputFile)).isdirectory();
+            if(!fs.statSync(path.dirname(outputFile)).isdirectory()){
+                mkdirp.sync(path.dirname(outputFile));
+            }
         }catch(e){
             mkdirp.sync(path.dirname(outputFile));
         }
@@ -125,7 +134,11 @@ var cdnplz = {
         if(!subResCdnUrl || !subResCdnUrl.length) return fileContent;
         subResCdnUrl.forEach(subRes => {
             for(var subResFileName in subRes) {
-                var replaceFileName = subResFileName.substring(this.options.static_path.length, subResFileName.length);
+                var subStart = this.options.static_path.length;
+                if(this._getFileSuffix(subResFileName) === 'css') {
+                    subStart = this.tempPath.length;
+                }
+                var replaceFileName = subResFileName.substring(subStart, subResFileName.length);
                 fileContent = fileContent.replace(new RegExp(replaceFileName, 'ig'), subRes[subResFileName]);
             }
         });
@@ -134,7 +147,11 @@ var cdnplz = {
 
     // 上传文件
     _uploadFile(fileName) {
-        fileName = this._getWholePathFile(fileName);
+        if(this._getFileSuffix(fileName) === 'css'){
+            fileName = this.tempPath+fileName;
+        }else {
+            fileName = this._getWholePathFile(fileName);
+        }
         if(this.cdnCache[fileName]) {  //判断本次是否已经上传过
             return this.cdnCache[fileName];
         }
@@ -149,7 +166,7 @@ var cdnplz = {
         }
         try{  // 上传
             console.log('上传文件'+fileName);
-            uploadPromise = this.cdnProvider[this.options.cdn_provider_upload].call(this, fileName, this.options.plugins[this.options.cdn_provider]);
+            uploadPromise = this.cdnProvider.upload(fileName, this.options.cdn_options);
             this.cdnCache[fileName] = uploadPromise;
             return uploadPromise;
         }catch(e){
